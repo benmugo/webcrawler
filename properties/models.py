c2516c9d6c3591d94be47e7de3f25b8abc0b7509
ben <benmugo1@gmail.com>
2019-06-07 19:57:41 +0300

    Crawling

--- properties/models.py
+++ properties/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Properties(models.Model):
-    location=models.CharField(max_length=250)
-    title=models.CharField(max_length=500)
-    #price=price.CharField(max_length=500)
-    #link=link.CharField(max_length=500)
-
+    location=models.CharField(max_length=250,blank=True,null=True)
+    title=models.CharField(max_length=500,blank=True,null=True)
+    price=models.CharField(max_length=500,blank=True,null=True)
+    link=models.CharField(max_length=500,blank=True,null=True)
+    image_path=models.CharField(max_length=2048,blank=True,null=True)
+    description=models.TextField(max_length=2000,blank=True,null=True)
+    bathroom=models.CharField(max_length=500,blank=True,null=True)
+    bedroom=models.CharField(max_length=500,blank=True,null=True)
+    area=models.CharField(max_length=500,blank=True,null=True)
+    
     def __str__(self):
         return self.title+'--'+self.location
--- properties/views.py
+++ properties/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render,HttpResponse
 import json
 from .models import Properties
+from .crawler import crawl_house
+import time
 
-# Create your views here.
-# Create your views here.
 def home(request):
     if request.method=="POST":
-        print("crawl here...")
-        with open("star2_formatted.json") as file:
+        file_path=str(int(time.time()*1000))+'star.json'
+        # url = 'https://www.the-star.co.ke/classifieds/house-apartment-for-rent/house--nairobi'
+        url=request.POST['url']
+        crawl  = crawl_house(url)
+        with open(file_path,"w") as f:
+            f.write(str(crawl)) 
+
+        f = open(file_path, "r+")
+        new_string=f.read().replace('\'','"')
+        f.write(new_string)
+
+        with open(file_path) as file:
             data=json.load(file)
             for prop in data:
                 p1=Properties()
                 p1.location=prop['location']
                 p1.title=prop['title']
-                #p1.price=prop['price']
-                #p1.link=prop['link']
+                p1.price=prop['price']
+                p1.link=prop['link']
+                p1.image_path=prop['path']
+                p1.description=prop['description']
+                #p1.bedroom=prop['bedroom']
+                #p1.area=prop['area']
+                #p1.bathroom=prop['bathroom']
                 p1.save()
-        return HttpResponse("Done inserting to db")
+        all_props=Properties.objects.all()
+        return render(request,"index.html",{'my_properties':all_props},None)
     else:
         all_props=Properties.objects.all()
         return render(request,"index.html",{'my_properties':all_props},None)
